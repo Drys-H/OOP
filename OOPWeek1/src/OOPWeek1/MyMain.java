f99d7f276cf5794563197a38001cc18ece3165bb
Drys-H <drysluiggi@gmail.com>
2025-10-11 19:11:17 +0100

    week1 done

--- OOPWeek1/src/OOPWeek1/MyMain.java
+++ OOPWeek1/src/OOPWeek1/MyMain.java
@@ -1,7 +1,19 @@
- package oopweek1;
+package OOPWeek1;
 
-public class MyNMain {
+import java.util.Date;
+
+public class MyMain {
     public static void main(String[] args) {
+        System.out.println("Hello Drys");
+        System.out.println("The date today is " + new Date());
 
+    Student s1 = new Student(1234567, "Mickey", "Mouse");
+    Student s2 = new Student(1234568, "Minnie", "Mouse");
+
+	System.out.println(s1.getIdNumber() + " " + s1.getFirstName() + " " + s1.getLastName());
+	System.out.println(s2.getIdNumber() + " " + s2.getFirstName() + " " + s2.getLastName());
+    System.out.println(s1.getFullName());
+    System.out.println(s2.getSortName());
+    System.out.println(s1);
     }
 }
--- OOPWeek1/src/OOPWeek1/Student.java
+++ OOPWeek1/src/OOPWeek1/Student.java
@@ -1,4 +1,35 @@
 package OOPWeek1;
 
 public class Student {
+    private int idNumber;
+    private String firstName;
+    private String lastName;
+
+    public Student(int idNumber, String first, String last) {
+        this.idNumber = idNumber;
+        this.firstName = first;
+        this.lastName = last;
+    }
+    public int getIdNumber() {
+        return this.idNumber;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    public String getSortName() {
+        return this.lastName + ", " + this.firstName;
+    }
+    public String toString() {
+        return this.idNumber + " " + getSortName();
+    }
 }
