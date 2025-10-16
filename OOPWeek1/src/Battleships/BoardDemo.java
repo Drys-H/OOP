446fa25bfe7f2a89033c7cfa0907ea9127f4684c
Drys-H <drysluiggi@gmail.com>
2025-10-16 12:30:10 +0100

    week2 done

--- OOPWeek1/src/Battleships/BoardDemo.java
+++ OOPWeek1/src/Battleships/BoardDemo.java
@@ -1,22 +1,6 @@
 package Battleships;
 
 public class BoardDemo {
-    // we also need to be able to print the board without showing
-    // the ships - this is one way to do that:
-    System.out.println("Showing the board with the ships hidden:");
-    String[] b = b1.toStringArray(false);
-    for(String r : b) {
-        System.out.println(r);
-    }
-    public static void main(String[] args) {
-        Board b1 = new Board(10,10);
-
-        b1.setup();
-
-        b1.getSquare(0,0).setTried();
-        b1.getSquare(0,1).setTried();
-
-        System.out.println(b1);
     public static void main(String[] args) {
         Board b1 = new Board(10,10);
 
@@ -43,5 +27,13 @@ public class BoardDemo {
         b1.dropBomb(2, 2);
         System.out.println(b1);
         System.out.println();
+
+        // we also need to be able to print the board without showing
+        // the ships - this is one way to do that:
+        System.out.println("Showing the board with the ships hidden:");
+        String[] b = b1.toStringArray(false);
+        for(String r : b) {
+            System.out.println(r);
+        }
     }
 }
